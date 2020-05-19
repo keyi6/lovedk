72a64de4124aeae020c76455f9aba3be577cc8f0
Keyi Li <keyi.li.9806@gmail.com>
2020-05-19 11:49:40 +0800

    fix: fix sequence

--- lovedk.py
+++ lovedk.py
@@ -31,33 +31,36 @@ async def run(username, password, DEBUG=False):
     # enter password
     await page.keyboard.press('Tab')
     await page.keyboard.type(password)
+    if DEBUG:
+        await page.screenshot({'path': 'step_1.png'})
 
     # click login
     await click('input.btn_1_new')
     print('login...')
+    if DEBUG:
+        await page.screenshot({'path': 'step_2.png'})
 
     # click the first questionnaire
     await click('ul.content li')
     print('enter the questionnaire...')
     if DEBUG:
-        await page.screenshot({'path': 'step_1.png'})
+        await page.screenshot({'path': 'step_3.png'})
 
     p = await page.Jeval('div.public_modal_tax', 'node => node.innerText')
     if p and '您在周期内已填写过此问卷' in p:
         print('⚠️', p)
-        return
+    else:
+        # click “确定”
+        await page.click('a.am-modal-button:nth-child(2)')
+        print('fill it with previous answer...')
+        if DEBUG:
+            await page.screenshot({'path': 'step_4.png'})
 
-    # click “确定”
-    await page.click('a.am-modal-button:nth-child(2)')
-    print('fill it with previous answer...')
-    if DEBUG:
-        await page.screenshot({'path': 'step_2.png'})
-
-    # click '提交'
-    await page.click('div.addanswer > div > div.btn_xs')
-    print('🎉 done!')
-    if DEBUG:
-        await page.screenshot({'path': 'step_3.png'})
+        # click '提交'
+        await page.click('div.addanswer > div > div.btn_xs')
+        print('🎉 done!')
+        if DEBUG:
+            await page.screenshot({'path': 'step_5.png'})
 
     await browser.close()
 
@@ -67,10 +70,13 @@ if __name__ == '__main__':
     password = ''
 
     if len(sys.argv) >= 2:
-        username = sys.argv[0]
-        password = sys.argv[1]
+        username = sys.argv[1]
+        password = sys.argv[2]
 
     if username == '' or password == '':
         print('⚠️ please enter username and password')
     else:
-        asyncio.get_event_loop().run_until_complete(run(username, password))
+        try:
+            asyncio.get_event_loop().run_until_complete(run(username, password))
+        except:
+            print('⚠️ 失败，请将 DEBUG 设置为 True 通过截图查看错误的地方')
