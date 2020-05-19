a7d6f70446882b24b2344e29170610c6bc3759b1
Keyi Li <keyi.li.9806@gmail.com>
2020-05-19 09:46:04 +0800

    feat: use puppeteer to automatically submit quesionarie

---
+++ README.md
@@ -0,0 +1,19 @@
+# fxxkdk
+
+此项目用于i大工健康上报自动打卡。
+
+## install
+
+```shell
+pip3 install -r requirements.txt
+```
+
+
+## config
+方法一：在 `fxxkdk.py` 的58-59行直接改变变量内容。
+
+方法二：使用命令行参数传入
+
+```shell
+python3 fxxkdk.py <your_username> <your_password>
+```
---
+++ fxxkdk.py
@@ -0,0 +1,74 @@
+import sys
+import time
+import asyncio
+from random import random
+from pyppeteer import launch
+
+
+DELAY = int(random() * 3 + 2)
+
+
+async def run(username, password, DEBUG=False):
+    url = 'https://api.m.dlut.edu.cn/login?redirect_uri=https%3a%2f%2flightapp.m.dlut.edu.cn%2fcheck%2fquestionnaire'
+    # init
+    browser = await launch()
+    page = await browser.newPage()
+    page.setDefaultNavigationTimeout(0)
+
+    # click func
+    async def click(selector):
+        await asyncio.gather(
+            page.waitForNavigation(),
+            page.click(selector),
+        )
+        time.sleep(DELAY)
+
+    await page.goto(url)
+
+    # enter username
+    await page.keyboard.press('Tab')
+    await page.keyboard.type(username)
+    # enter password
+    await page.keyboard.press('Tab')
+    await page.keyboard.type(password)
+
+    # click login
+    await click('input.btn_1_new')
+    print('login success')
+
+    # click the first questionnaire
+    await click('ul.content li')
+    print('successfully enter the questionnaire')
+    if DEBUG:
+        await page.screenshot({'path': 'step_1.png'})
+
+    # click “确定”
+    await page.click('a.am-modal-button:nth-child(2)')
+    print('successfully fill it with previous answer')
+    if DEBUG:
+        await page.screenshot({'path': 'step_2.png'})
+
+    # click '提交'
+    await page.click('div.addanswer > div > div.btn_xs')
+    print('🎉 done')
+    if DEBUG:
+        await page.screenshot({'path': 'step_3.png'})
+
+    await browser.close()
+
+
+if __name__ == '__main__':
+    username = ''
+    password = ''
+
+    if len(sys.argv) >= 2:
+        username = sys.argv[0]
+        password = sys.argv[1]
+
+    if username == '' or password == '':
+        print('⚠️ please enter username and password')
+
+    try:
+        asyncio.get_event_loop().run_until_complete(run(username, password))
+    except:
+        print('\n⚠️ faild, trying again...\n')
